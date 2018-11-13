17f1ab5c26910cc829b258c2bee77546857135f4
ryanmcc13 <ryanmccullough21@gmail.com>
2018-11-12 23:39:33 -0600

    initial commit

---
+++ PA3/src/stuff/Main.java
@@ -0,0 +1,12 @@
+package stuff;
+
+public class Main {
+
+	public static void main(String[] args) {
+		String[] topics = {"tennis", "grand slam"};
+		WikiCrawler w = new WikiCrawler("/wiki/Tennis", topics, 100, "WikiTennisGraph.txt", true);
+		w.crawl();
+
+	}
+
+}
---
+++ PA3/src/stuff/SpamFarm.java
@@ -0,0 +1,17 @@
+package stuff;
+
+import java.io.File;
+
+public class SpamFarm {
+
+	File graphEdges;
+	int target;
+	public SpamFarm(File graphEdges,int target) {
+		this.graphEdges=graphEdges;
+		this.target=target;
+	}
+	
+	public void createSpam(String fileName) {
+		
+	}
+}
